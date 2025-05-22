3f57cc65dc9b77be6128b8f1828b0023e9ce9669
phkmolina1986 <pedro.kuchak@gmail.com>
2025-05-22 10:00:08 -0600

    Updated the flow of the devolpment as comments

--- SODV1202_ConnectFour/Program.cs
+++ SODV1202_ConnectFour/Program.cs
@@ -1,7 +1,29 @@
-﻿public class Program
+﻿using static System.Net.Mime.MediaTypeNames;
+using static System.Reflection.Metadata.BlobBuilder;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+//CLASS PLAYER
+
+//CLASS PLAYER HUMAN
+
+//CLASS PLAYER COMPUTER
+
+//CLASS GAME
+
+//CLASS BOARD
+
+public class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //GAME LOOP
+            //Start game screen
+            //Players handle methods
+            //Game play
+            //Game over screen
+            //Pplay again?
+
     }
 } 
